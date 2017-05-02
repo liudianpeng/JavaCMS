00cd1128bd7f81e104826aa954631907aab62bc8
Amonhen <elessar4531@me.com>
2017-05-02 10:42:03 +0300

    sssssasds

---
+++ src/main/java/testBlog/controller/ChatController.java
@@ -0,0 +1,18 @@
+package testBlog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    @GetMapping("/")
+    public String getMainChatPage(Model model) {
+        model.addAttribute("view","chat/main");
+        return "base-layout";
+    }
+
+}
